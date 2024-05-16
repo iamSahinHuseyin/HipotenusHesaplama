64d85d4646a7f3b057cfda502ce52f74a8274747
sahin <sahinhuseyinn0307@gmal.com>
2024-05-16 22:52:42 +0300

    Bitis Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Kullanıcıdan alınan üçgen kenar uzunluklarına göre dik kenar uzunluğu bulmak.(Hip. hesaplama)
+        //Kullanıcıdan alınan üçgen kenar uzunluklarına göre dik kenar uzunluğu bulmak.(Hip. hesaplama).
+        Scanner sc = new Scanner(System.in);
+        int kenar1;
+        int kenar2;
+        double uzunKenar;
 
+        System.out.print("Kenar 1'i giriniz: ");
+        kenar1 = sc.nextInt();
+        System.out.print("Kenar 2'i giriniz: ");
+        kenar2 = sc.nextInt();
+        uzunKenar = Math.sqrt((kenar1 * kenar1 + kenar2 * kenar2));
+        System.out.println("Uzun Kenar: " + uzunKenar);3
         }
     }
